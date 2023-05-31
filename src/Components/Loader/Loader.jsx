77877710c5db8e0be51be3bd5cb1526c846562de
Neat <sophaneat@student.fdu.edu>
2023-05-30 22:09:04 -0700

    Add generate image on CityPage

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div className="loader"></div>
+        <div class="loader">
+            <div class="snippet" data-title="dot-flashing">
+            <div class="stage">
+                <div class="dot-flashing"></div>
+            </div>
+            </div>
+        </div>
     )
 }
 
--- src/Pages/CityPage/CityPage.jsx
+++ src/Pages/CityPage/CityPage.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import {useParams} from "react-router-dom";
+import "./CityPage.scss";
+import axios from "axios";
+import defaultBG from "../../assets/images/cityBackground.jpg";
+// Components
+import Loader from '../../Components/Loader/Loader';
 const CityPage = () => {
+    const {cityname} = useParams();
+    const [backgroundImg, setBackgroundImg] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    // Fetch Background Image
+    useEffect(() => {
+        fetchImage();
+    }, [])
+    
+    // Handler Functions
+    const fetchImage = async () => {
+        try{
+            setIsLoading(true);
+            const url = `https://api.unsplash.com/photos/random/?query=${cityname}&orientation=landscape&count=1`;
+            const response = await axios.get(url, 
+                            {
+                                headers: {
+                                    "Accept-Version": "v1",
+                                    "Authorization": `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+                                }
+                            });
+            setBackgroundImg(response.data[0].urls.full);
+        } catch(error) {
+            console.log(error);
+        } 
+    }
     return (
-        <div>CityPage</div>
+        <div className="city">
+            <section className="city__hero">
+                <img onLoad={()=>setIsLoading(false)} className="city__hero__img" src={backgroundImg ? backgroundImg : defaultBG} alt={cityname} />
+                <div className="city__hero__content">
+                    <h1 className="city__hero__title">Let's discover {cityname}</h1>
+                    <button onClick={() => fetchImage()} className="city__hero__btn">Get a new image</button>
+                    {isLoading && <Loader />}
+                </div>  
+            </section>
+        </div>
     )
 }
 
